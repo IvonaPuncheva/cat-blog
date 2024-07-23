71b16c0406fe1601346cdd6b0afa0bfd449f4a84
IvonaPuncheva <ivpuncheva@gmail.com>
2024-07-23 21:28:18 +0300

    Add page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+
 import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Register from "./components/register/Register"
+import Login from "./components/login/Login"
+import CatList from "./components/cat-list/CatList"
+import CatCreate from "./components/cat-create/CatCreate"
 
 function App() {
 
@@ -8,6 +15,14 @@ function App() {
 
 
 <main id="main-content">
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
+    <Route path='/cats' element={<CatList />} />
+    <Route path='/cats/create' element={<CatCreate />} />
+
+  </Routes>
 </main>
     </div>
   )
