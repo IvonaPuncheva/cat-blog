3501153a299be3b0445e1259d95da8f4b3ca6af5
IvonaPuncheva <ivpuncheva@gmail.com>
2024-07-24 23:32:20 +0300

    Remove saved password in auth state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,15 +1,15 @@
 import { useContext } from "react";
 
-import { login } from "../api/authAPI"
+import { login, register } from "../api/authAPI"
 import { AuthContext } from "../context/AuthContext";
 
 export const useLogin = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const {password: _, authData} = await login(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
         return result;
 
@@ -21,9 +21,9 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await registerHandler(email, password);
+        const {password: _, ...authData} = await register(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
         return result;
     };
