4d318d7d81e1ef5c118ac107f67b38232ed2bb43
IvonaPuncheva <ivpuncheva@gmail.com>
2024-08-15 23:54:41 +0300

    Update Footer.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () =>{
     return(
         <footer className="bg-gray-800 text-white py-4 text-center">
         <p className="text-sm">
-          Copyright &copy; 2023 <span className="font-semibold">ElectraSwap</span>, All rights reserved.
+          Copyright &copy; 2024 <span className="font-semibold">Cats blog</span>, All rights reserved.
         </p>
       </footer>
     )
